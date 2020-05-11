8db15808ae4901403914fd02accb98d051ab6891
justinyeh <justinyeh1995@gmail.com>
2020-05-11 18:38:43 +0800

    Lab3 Report added

--- Computer-Organization-Lab3/alu.v
+++ Computer-Organization-Lab3/alu.v
@@ -17,11 +17,6 @@ module alu(
 
 /* Write your code HERE */
 
-wire   [32-1:0] result_wire;
-wire            carryout;
-wire            overflow_wire;
-
-
 always @* 
    begin
       if (rst_n) begin          
@@ -34,61 +29,61 @@ always @*
 		4'b0000: 
 			begin 
 				result <= src1 & src2;
-				zero <= (result == 0)?1:0; 
+				zero <= (src1 == src2)?1:0; 
 			end
 		/* OR :1*/	
 		4'b0001: 
 			begin 
 				result <= src1 | src2; 
-				zero <= (result == 0)?1:0;  
+				zero <= (src1 == src2)?1:0;  
 			end
 		/* ADD :2*/
 		4'b0010: 
 			begin
 				result <= src1 + src2;
-				zero <= (result == 0)?1:0; 
+				zero <= (src1 == src2)?1:0; 
 			end
 		/* SUB :6*/
 		4'b0110: 
 			begin
 				result <= src1 - src2;
-				zero <= (result == 0)?1:0; 	
+				zero <= (src1 == src2)?1:0; 	
 			end
 		/* SLT :7*/
 		4'b0111: 
 			begin
 				result <= (src1 < src2)?1:0;
-				zero <= (result == 0)?1:0; 	   
+				zero <= (src1 == src2)?1:0; 	   
 			end
 		/* NOR :12*/
 		4'b1100: 
 			begin
 				result <= ~(src1 | src2);
-				zero <= (result == 0)?1:0; 	
+				zero <= (src1 == src2)?1:0; 	
 			end
 		/* XOR :13*/
 		4'b1101: 
 			begin
 				result <= src1 ^ src2;
-				zero <= (result == 0)?1:0; 
+				zero <= (src1 == src2)?1:0; 
 			end
 		/* SLL :3*/
 		4'b0011: 
 			begin
 				result <= src1 << src2;
-				zero <= (result == 0)?1:0; 
+				zero <= (src1 == src2)?1:0; 
 			end
 		/* SRL :4*/
 		4'b0100: 
 			begin
 				result <= src1 >> src2;
-				zero <= (result == 0)?1:0; 
+				zero <= (src1 == src2)?1:0; 
 			end
 		/* SRA :5*/
 		4'b0101: 
 			begin
 				result <= src1 >>> src2;
-				zero <= (result == 0)?1:0; 
+				zero <= (src1 == src2)?1:0; 
 			
 			end
 
@@ -96,14 +91,14 @@ always @*
 		4'b1010: 
 			begin
 				result <= src1 - src2;
-				zero <= (result == 0)?1:0; 
+				zero <= (src1 == src2)?1:0; 
 			end
 
 		/* BNE :11*/
 		4'b1011: 
 			begin
 				result <= src1 - src2;
-				zero <= (result == 0)?0:1;
+				zero <= (src1 == src2)?0:1;
 			
 			end
 		
